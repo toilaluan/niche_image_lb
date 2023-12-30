9f4d9520723f45a9c49bbd7020ebfb48ba18e82f
Ubuntu <ubuntu@ip-172-31-6-241.ca-central-1.compute.internal>
2023-12-30 07:17:35 +0000

    update

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ sudo docker run \
    -v ./haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg \
    -p 15000:15000 \
    -p 15001:15001 \
-   -p 15002:15002 \
-   -p 14999:14999 \
+   -p 15010:15010 \
+   -p 15011:15011 \
+   -p 15012:15012 \
    haproxy
