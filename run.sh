03c710d77df344467c6eff7894da6df07cea30b6
Ubuntu <ubuntu@ip-172-31-6-241.ca-central-1.compute.internal>
2024-01-16 15:57:25 +0000

    update

--- run.sh
+++ run.sh
@@ -4,9 +4,12 @@ sudo docker run \
    --name haproxy \
    --net nichenet \
    -v ./haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg \
+   -v ./nicheimage.pem:/etc/ssl/certs/nicheimage.pem \
+   -p 17000:17000 \
    -p 15000:15000 \
    -p 15001:15001 \
    -p 15010:15010 \
    -p 15011:15011 \
    -p 15012:15012 \
+   -p 15013:15013 \
    haproxy
